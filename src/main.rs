2501bd8e3ea3dcb6725c1df0338da39e0b632361
Amy Y <ctrlalternativedelete@gmail.com>
2022-04-10 05:57:01 +1000

    moved main to be cleaner

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod opcode_parser;
 mod fstools;
 mod input;
 
+use glium::Display;
 use glium::glutin::event::{VirtualKeyCode, ElementState};
 
 use crate::fstools::get_file_as_byte_vec;
 use crate::chip8::Chip8;
+use crate::input::parse_input;
 
 extern crate glium;
 
@@ -43,20 +45,7 @@ fn main() {
         // cycle cpu
         chip8inst.single_cycle();
         
-        // render frame
-        let mut disptexturevec = vec![vec![(0u8, 0u8, 0u8); 64]; 32];
-        for (i, e) in  chip8inst.display.iter().enumerate() {
-            if *e == 1 {
-                disptexturevec[31 - (i % 32)][i / 32] = (255u8, 255u8, 255u8);
-            }
-        }
-        let texture = glium::Texture2d::new(&display, disptexturevec).unwrap();
-        // get texture from display
-
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Nearest);
-        target.finish().unwrap();
+        render_texture_to_target(&chip8inst.display, &display);
 
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         match ev {
@@ -67,11 +56,28 @@ fn main() {
                 },
                 glutin::event::WindowEvent::KeyboardInput { device_id, input, is_synthetic } => {
                     // println!("{:?}", input.virtual_keycode.unwrap());
-                    crate::input::parse_input(input, &mut chip8inst);
+                    parse_input(input, &mut chip8inst);
                 },
                 _ => return,
             },
             _ => (),
         }
     });
+}
+
+fn render_texture_to_target(dispmem: &[u8; 2048], display: &glium::Display) {
+    use crate::glium::Surface;
+
+    let mut disptexturevec = vec![vec![(0u8, 0u8, 0u8); 64]; 32];
+    for (i, e) in  dispmem.iter().enumerate() {
+        if *e == 1 {
+            disptexturevec[31 - (i % 32)][i / 32] = (255u8, 255u8, 255u8);
+        }
+    }
+    let texture = glium::Texture2d::new(display, disptexturevec).unwrap();
+
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 0.0, 1.0);
+    texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Nearest);
+    target.finish().unwrap();
 }
