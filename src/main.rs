91b99c2f1492ffe496b7e18023b0ef2a88971ffd
amy <ctrlalternativedelete@gmail.com>
2022-04-09 17:55:59 +1000

    added

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ fn main() {
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     event_loop.run(move |ev, _, control_flow| {
+        // set next run time to 1 second / 500hz =  2 milliseconds.
+        let next_frame_time = std::time::Instant::now() +std::time::Duration::from_millis(2);
+
+        // cycle cpu
         chip8inst.single_cycle();
         
         // render frame
@@ -35,11 +39,8 @@ fn main() {
         target.clear_color(0.0, 0.0, 0.0, 1.0);
         texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Nearest);
         target.finish().unwrap();
-        
-        // let next_frame_time = std::time::Instant::now() +
-        //     std::time::Duration::from_nanos(16_666_667);
 
-        // *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         match ev {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
