960e88b65db8feccb86ffe02f14f13b1cc42076a
Amy Y <ctrlalternativedelete@gmail.com>
2022-04-11 09:45:40 +1000

    changed keymap implementation

--- src/input.rs
+++ src/input.rs
@@ -2,7 +2,7 @@ use glium::glutin::event::{KeyboardInput, ElementState, VirtualKeyCode};
 
 use crate::chip8::Chip8;
 
-static KEYMAP: [u8; 16] = [
+static KEYMAP: [usize; 16] = [
     0x1, // 1
     0x2, // 2
     0x3, // 3
@@ -25,55 +25,55 @@ pub fn parse_input(input: KeyboardInput, chip8inst: &mut Chip8) {
     let pressed = (input.state == ElementState::Pressed) as u8;
     match input.virtual_keycode.unwrap_or(VirtualKeyCode::L) {
         VirtualKeyCode::Key1=> {
-            chip8inst.keystate[0x1] = pressed;
+            chip8inst.keystate[KEYMAP[0]] = pressed;
         },
         VirtualKeyCode::Key2=> {
-            chip8inst.keystate[0x2] = pressed;
+            chip8inst.keystate[KEYMAP[1]] = pressed;
         },
         VirtualKeyCode::Key3=> {
-            chip8inst.keystate[0x3] = pressed;
+            chip8inst.keystate[KEYMAP[2]] = pressed;
         },
         VirtualKeyCode::Key4=> {
-            chip8inst.keystate[0xC] = pressed;
+            chip8inst.keystate[KEYMAP[3]] = pressed;
         },
 
         VirtualKeyCode::Q=> {
-            chip8inst.keystate[0x4] = pressed;
+            chip8inst.keystate[KEYMAP[4]] = pressed;
         },
         VirtualKeyCode::W=> {
-            chip8inst.keystate[0x5] = pressed;
+            chip8inst.keystate[KEYMAP[5]] = pressed;
         },
         VirtualKeyCode::E=> {
-            chip8inst.keystate[0x6] = pressed;
+            chip8inst.keystate[KEYMAP[6]] = pressed;
         },
         VirtualKeyCode::R=> {
-            chip8inst.keystate[0xD] = pressed;
+            chip8inst.keystate[KEYMAP[7]] = pressed;
         },
 
         VirtualKeyCode::A=> {
-            chip8inst.keystate[0x7] = pressed;
+            chip8inst.keystate[KEYMAP[8]] = pressed;
         },
         VirtualKeyCode::S=> {
-            chip8inst.keystate[0x8] = pressed;
+            chip8inst.keystate[KEYMAP[9]] = pressed;
         },
         VirtualKeyCode::D=> {
-            chip8inst.keystate[0x9] = pressed;
+            chip8inst.keystate[KEYMAP[10]] = pressed;
         },
         VirtualKeyCode::F=> {
-            chip8inst.keystate[0xE] = pressed;
+            chip8inst.keystate[KEYMAP[11]] = pressed;
         },
 
         VirtualKeyCode::Z=> {
-            chip8inst.keystate[0xA] = pressed;
+            chip8inst.keystate[KEYMAP[12]] = pressed;
         },
         VirtualKeyCode::X=> {
-            chip8inst.keystate[0x0] = pressed;
+            chip8inst.keystate[KEYMAP[13]] = pressed;
         },
         VirtualKeyCode::C=> {
-            chip8inst.keystate[0xB] = pressed;
+            chip8inst.keystate[KEYMAP[14]] = pressed;
         },
         VirtualKeyCode::V=> {
-            chip8inst.keystate[0xF] = pressed;
+            chip8inst.keystate[KEYMAP[15]] = pressed;
         },
 
         _ => {}
